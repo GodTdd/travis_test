1df42d529e9ad9a48f764c06a3c0c2cdbdea1b1e
Tatu Aalto <aalto.tatu@gmail.com>
2016-12-13 00:06:51 +0200

    foo

--- run_me.py
+++ run_me.py
@@ -2,7 +2,10 @@ import os
 import sys
 
 print 'start'
-
+f = open('super_secret.txt.enc', 'r')
+for line in f.readlines():
+    print line
+f.close()
 print os.environ.get('SAUCE_ACCESS_KEY')
 print os.environ.get('SAUCE_USERNAME')
 
